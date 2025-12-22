1a199abaf49cfbf47a8fd6ba6d31eb8400fcbbd3
intmainale <alessandro.savio04@gmail.com>
2025-12-22 16:47:05 +0100

    .

--- php/dettagli_proprieta.php
+++ php/dettagli_proprieta.php
@@ -1,1 +1,3 @@
-
+<!-- fa andare l'utente in dettagli.html in base al suo status: user/admin
+ in base allo status cambia il contenuto della pagina:  user -> mostra i dettagli della proprietà singola
+                                                        admin -> mostra un form con le cose da poter modificare  -->
--- php/proprieta.php
+++ php/proprieta.php
@@ -1,1 +1,3 @@
-
+<!-- fa andare l'utente in proprieta.html in base al suo status: user/admin
+ in base allo status cambia il contenuto della pagina:  user -> mostra tutte le proprietà con pulsante view details
+                                                        admin -> mostra tutte le proprietà con pulsanti edit/remove e un pulsante add in alto a dx  -->
