78880c2374e85616c88e82208dbd33bc7fc4113e
intmainale <alessandro.savio04@gmail.com>
2025-12-22 16:37:39 +0100

    .

--- php/showcase.php
+++
@@ -1,6 +0,0 @@
-<!-- nello stesso file si fa:
- if(amministratore)
- mostro le proprietà cercate tramite il filtro con il bottome edit/remove come azioni
- 
- else if(utente comune)
- mostro solamente le proprietà senza pulsanti di modifica o eliminazione-->
