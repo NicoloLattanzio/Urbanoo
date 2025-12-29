ddd64bf3cdc63d25a1a828d6a3dcff401d7c169c
AndreaSgreva <andrea.sgreva@studenti.unipd.it>
2025-12-29 18:39:47 +0100

    sistemato database.sql

--- database/database.sql
+++ database/database.sql
@@ -1,34 +1,33 @@
 CREATE TABLE utenti (
-    id SERIAL PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(20) NOT NULL,
     cognome VARCHAR(20) NOT NULL,
     email VARCHAR(50) UNIQUE NOT NULL,
-    password VARCHAR(30) NOT NULL,
+    password VARCHAR(255) NOT NULL,
     telefono VARCHAR(22),
     ruolo VARCHAR(5) NOT NULL DEFAULT 'user',
     data_registrazione TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    CHECK (ruolo IN ('user', 'admin'))
+    CONSTRAINT chk_ruolo CHECK (ruolo IN ('user', 'admin'))
 );
 
-CREATE TABLE immobili (
-    id SERIAL PRIMARY KEY,
-    titolo VARCHAR(100) NOT NULL,
+CREATE TABLE proprieta (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
     descrizione TEXT,
     tipologia VARCHAR(20) NOT NULL,
-    CHECK (tipologia IN ('appartamento', 'villa', 'ufficio', 'terreno', 'garage')),
+    CONSTRAINT chk_tipologia CHECK (tipologia IN ('Monolocale', 'Bilocale', 'Trilocale', 'Villa', 'Attico', 'Rustico')), 
     indirizzo VARCHAR(150) NOT NULL,
     citta VARCHAR(100) NOT NULL,
     prezzo INT NOT NULL,
-    CHECK (prezzo > 0),
+    CONSTRAINT chk_prezzo CHECK (prezzo > 0),
     metri_quadri INT NOT NULL,
-    CHECK (metri_quadri > 0),
+    CONSTRAINT chk_mq CHECK (metri_quadri > 0),
     locali INT NOT NULL,
-    CHECK (locali > 0),
-    immagine_copertina VARCHAR(255),
+    CONSTRAINT chk_locali CHECK (locali > 0),
+    immagine VARCHAR(255), -- Percorso dell'immagine
     data_inserimento TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     disponibile BOOLEAN NOT NULL DEFAULT TRUE
 );
 
-
-CREATE INDEX idx_immobili_citta ON immobili(citta);
-CREATE INDEX idx_immobili_prezzo ON immobili(prezzo);
+CREATE INDEX idx_proprieta_citta ON proprieta(citta);
+CREATE INDEX idx_proprieta_prezzo ON proprieta(prezzo);
