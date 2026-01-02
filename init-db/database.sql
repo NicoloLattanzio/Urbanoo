0ebbcfae9e4501038effdf33f8a10c0b2f89dfd4
intmainale <alessandro.savio04@gmail.com>
2026-01-02 15:10:06 +0100

    .

--- init-db/database.sql
+++ init-db/database.sql
@@ -39,7 +39,7 @@ INSERT INTO utenti (nome, cognome, email, password, ruolo) VALUES
         'Admin',
         'Test',
         'admin@urbanoo.com',
-        '$2y$10$6lnIBHp6loYxZap2bQ6bnOJ57zOvcsvZd.BJer8ZSv1y1V3sTaR3.', // hashed password for 'password1234'
+        '$2y$10$6lnIBHp6loYxZap2bQ6bnOJ57zOvcsvZd.BJer8ZSv1y1V3sTaR3.', -- hashed password for 'password1234'
         'admin'
     );
 
