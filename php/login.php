59ac656754bf64b1b6e3d530d2db50bd60131073
intmainale <alessandro.savio04@gmail.com>
2026-01-07 18:14:10 +0100

    Fixed error flow, readable code

--- php/login.php
+++ php/login.php
@@ -1,73 +1,89 @@
 <?php
-    session_start();
-    require_once 'dbconnection.php';
-    use DB\DBAccess;
-    $formValido = true;
-    $email = '';
-    $emailErr = '';
-    $password = '';
-    $passwordErr = '';
+session_start();
+require_once 'dbconnection.php';
+use DB\DBAccess;
+$formValido = true;
+$email = '';
+$emailErr = '';
+$password = '';
+$passwordErr = '';
 
-    $PageAreariservata = file_get_contents('areariservata.php');
-    $PageLogin = file_get_contents('../html/login.html');
-    $connessione = new DBAccess();
-    $connessioneOK = $connessione->openDBConnection();
-    if(!$connessioneOK){
-        header("location: /500.html");
+$PageAreariservata = file_get_contents('areariservata.php');
+$PageLogin = file_get_contents('../html/login.html');
+$connessione = new DBAccess();
+$connessioneOK = $connessione->openDBConnection();
+if(!$connessioneOK){
+    //DB connection error
+    header("location: /500.html");
+    exit();
+}
+/*
+    1. user already logged in as user       -> redirect to areariservata.php
+    2. user already logged in as admin      -> redirect to areariservata.php
+    3. user not logged in and form sent     -> login and validation     
+    4. user not logged in and form not sent -> show login form (login.html)
+*/
+if (isset($_SESSION['user_id'], $_SESSION['role'])) {   //1,2
+    header("Location: areariservata.php");
+    exit;
+}
+else{
+    if($_SERVER['REQUEST_METHOD'] !== 'POST'){
+        //4
+        echo $PageLogin;
+    }
+
+    //3
+    /*
+        Essential input validation is done client side only for better UX
+    */
+    $email = $_POST['email'] ?? '';
+    $password = $_POST['password'] ?? '';
+    /*
+        DB output management:
+        -> [true, $row]: query returned a result        ->  user exists, 
+                                                            store session vars + 
+                                                            redirect to areariservata.php   1)
+        -> [true, null]: query returned an empty result ->  user not found, 
+                                                            wrong credentials + 
+                                                            redirect to areariservata.php   2)
+        -> [false, DB_ERROR]: query failed              ->  500.html                        3)
+    */
+    $result = $connessione->getUser($email);
+    if(!$result['success']){
+        //3)
+        header('location: /500.html');
         exit();
     }
-    /*
-        1. user already logged in as user -> redirect to areariservata.php
-        2. user already logged in as admin -> redirect to areariservata.php
-        3. user not logged in and form sent -> process login and validation -> if success redirect to areariservata.php, else show login form with errors
-        4. user not logged in and form not sent -> show login form (login.html)
-    */
-    if (isset($_SESSION['user_id'], $_SESSION['role'])) {   //1,2
-        header("Location: areariservata.php");
-        exit;
+    $user = $result['content'];
+    if(!$user){
+        //2)
+        $emailErr = '<p><span lang="en">Email</span> non trovata.</p>';
+        $formValido = false;
+    }
+    if(password_verify($password, $user['password'])){
+        //1)
+        $_SESSION['name'] = $user['nome'];
+        $_SESSION['user_id'] = $user['id'];
+        $_SESSION['email'] = $user['email'];
+        $_SESSION['role'] = $user['ruolo'];
     }
     else{
-        if($_SERVER['REQUEST_METHOD'] === 'POST'){  //3
-            
-            $email = $_POST['email'] ?? '';
-            $password = $_POST['password'] ?? '';
-            
-            $result = $connessione->getUser($email);
-            if($result['success']){
-                if($result['content']){
-                    $user = $result['content'];
-                    if(password_verify($password, $user['password'])){
-                        $_SESSION['name'] = $user['nome'];
-                        $_SESSION['user_id'] = $user['id'];
-                        $_SESSION['email'] = $user['email'];
-                        $_SESSION['role'] = $user['ruolo'];
-                    }
-                    else{
-                        $passwordErr .= '<p><span lang="en">Password</span> non corretta.</p>';
-                        $formValido = false;
-                    }
-                }
-                else{
-                    $emailErr = '<p><span lang="en">Email</span> non trovata.</p>';
-                    $formValido = false;
-                }
-            } else {
-                header('location: /500.html');
-                exit();
-            }
-
-            if($formValido){ //login successful
-                header("Location: areariservata.php");
-                exit();
-            }
-            else{   //login failed, show login form with errors
-                $PageLogin = str_replace('[email_err]', $emailErr, $PageLogin);
-                $PageLogin = str_replace('[password_err]', $passwordErr, $PageLogin);
-                echo $PageLogin;
-            }
-        }
-        else{   //4
-            echo $PageLogin;
-        }
+        //2)
+        $passwordErr .= '<p><span lang="en">Password</span> non corretta.</p>';
+        $formValido = false;
     }
+
+    if($formValido){ 
+        //login successful
+        header("Location: areariservata.php");
+        exit();
+    }
+    else{
+        //login failed, show login form with errors
+        $PageLogin = str_replace('[email_err]', $emailErr, $PageLogin);
+        $PageLogin = str_replace('[password_err]', $passwordErr, $PageLogin);
+        echo $PageLogin;
+    }
+}
 ?>
--- php/modifica_password.php
+++ php/modifica_password.php
@@ -3,8 +3,13 @@ session_start();
 require_once "dbconnection.php";
 use DB\DBAccess;
 
-if (!isset($_SESSION['email'])) { // l'utente deve essere loggato
-    header("Location: login.php"); 
+/*
+    User check:
+    user -> change psw
+    none -> redirect to 403.html
+*/
+if (!isset($_SESSION['email'])) {
+    header("Location: /403.html"); 
     exit();
 }
 
@@ -17,94 +22,97 @@ $old_pswErr = "";
 $new_pswErr = "";
 $formValido = true;
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $email = trim($_POST['email'] ?? '');
-    $old   = $_POST['old_password'] ?? '';
-    $new   = $_POST['new_password'] ?? '';
-    $email = trim($_POST['email'] ?? '');
+/*
+    1. user visit modifica_password.php via link      -> redirect to areariservata.php
+    2. user visit modifica_password.php via form      -> redirect to areariservata.php
+*/
+if ($_SERVER["REQUEST_METHOD"] !== "POST"){
+    echo $paginaHTML;
+    exit(); //????? serve
+}
+$email = trim($_POST['email'] ?? '');
+$old   = $_POST['old_password'] ?? '';
+$new   = $_POST['new_password'] ?? '';
+$email = trim($_POST['email'] ?? '');
 
-    //prima validazione dei campi
-    if($email === '') {
-        $emailErr .= '<p><span lang="en">Email</span> non inserita</p>';
-        $formValido = false;
-    } else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) { //aggiungere controllo = delle due password
-        $emailErr .= '<p>Inserire un <span lang="en">email</span> valida.</p>';
-        $formValido = false;
-    }
-    if($old === ''){
-        $old_pswErr .= '<p><span lang="en">Password</span> attuale non inserita</p>';
-        $formValido = false;
-    }
-    if($new === ''){
-        $new_pswErr .= '<p>Nuova <span lang="en">password</span> non inserita</p>';
-        $formValido = false;
-    }
+//prima validazione dei campi
+if($email === '') {
+    $emailErr .= '<p><span lang="en">Email</span> non inserita</p>';
+    $formValido = false;
+} else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) { //aggiungere controllo = delle due password
+    $emailErr .= '<p>Inserire un <span lang="en">email</span> valida.</p>';
+    $formValido = false;
+}
+if($old === ''){
+    $old_pswErr .= '<p><span lang="en">Password</span> attuale non inserita</p>';
+    $formValido = false;
+}
+if($new === ''){
+    $new_pswErr .= '<p>Nuova <span lang="en">password</span> non inserita</p>';
+    $formValido = false;
+}
 
-    //secondi controlli dei campi tramite db
-    if($formValido) {
-        $connessione = new DBAccess();
-        $connessioneOK = $connessione->openDBConnection();
-        if ($connessioneOK) {
-            if($email !== $_SESSION["email"]){
-                $emailErr .= "<p>L'<span lang='en'>email</span> inserita non corrisponde a quella del tuo <span lang='en'>account</span>.</p>";
+//secondi controlli dei campi tramite db
+if($formValido) {
+    $connessione = new DBAccess();
+    $connessioneOK = $connessione->openDBConnection();
+    if ($connessioneOK) {
+        if($email !== $_SESSION["email"]){
+            $emailErr .= "<p>L'<span lang='en'>email</span> inserita non corrisponde a quella del tuo <span lang='en'>account</span>.</p>";
+            $formValido = false;
+        } 
+        if($formValido) { //se la email è valida ed uguale a quella dell'account allora procedo con altre validazioni
+            $oldpswResult = $connessione->checkOldPassword($email, $old);
+            if($oldpswResult['success'] && $oldpswResult['content'] == "PASSWORD_INVALID") {
+                $old_pswErr .= "<p>La <span lang='en'>password</span> inserita non corrisponde a quella del tuo <span lang='en'>account</span>.</p>";
                 $formValido = false;
-            } 
-            if($formValido) { //se la email è valida ed uguale a quella dell'account allora procedo con altre validazioni
-                $oldpswResult = $connessione->checkOldPassword($email, $old);
-                if($oldpswResult['success'] && $oldpswResult['content'] == "PASSWORD_INVALID") {
-                    $old_pswErr .= "<p>La <span lang='en'>password</span> inserita non corrisponde a quella del tuo <span lang='en'>account</span>.</p>";
-                    $formValido = false;
-                } else if(!$oldpswResult['success']){
-                    header("Location: /500.html");
-                    exit();
-                }
-                //validazione new password
-                if (strlen($new) < 6) {
-                    $new_pswErr .= '<p>La <span lang="en">password</span> deve contenere almeno 6 caratteri</p>';
-                    $formValido = false;
-                }
-            } else { //email valida ma non uguale a quella dell'account -> mostro già gli errori
-                $paginaHTML = str_replace('[email_err]', $emailErr, $paginaHTML);
-                $paginaHTML = str_replace('[old_password_err]', $old_pswErr, $paginaHTML);
-                $paginaHTML = str_replace('[new_password_err]', $new_pswErr, $paginaHTML);
-                echo $paginaHTML;
+            } else if(!$oldpswResult['success']){
+                header("Location: /500.html");
+                exit();
             }
-        } else { //problemi con la connessione al DB
-            header("Location: /500.html");
-            exit();
+            //validazione new password
+            if (strlen($new) < 6) {
+                $new_pswErr .= '<p>La <span lang="en">password</span> deve contenere almeno 6 caratteri</p>';
+                $formValido = false;
+            }
+        } else { //email valida ma non uguale a quella dell'account -> mostro già gli errori
+            $paginaHTML = str_replace('[email_err]', $emailErr, $paginaHTML);
+            $paginaHTML = str_replace('[old_password_err]', $old_pswErr, $paginaHTML);
+            $paginaHTML = str_replace('[new_password_err]', $new_pswErr, $paginaHTML);
+            echo $paginaHTML;
         }
-    } 
-    
-    else { // il form è gia non valido verificando solo i primi controlli
-        $paginaHTML = str_replace('[email_err]', $emailErr, $paginaHTML);
-        $paginaHTML = str_replace('[old_password_err]', $old_pswErr, $paginaHTML);
-        $paginaHTML = str_replace('[new_password_err]', $new_pswErr, $paginaHTML);
-        echo $paginaHTML;
+    } else { //problemi con la connessione al DB
+        header("Location: /500.html");
+        exit();
     }
+} 
 
-    if($formValido) { //modifica psw solo se tutte le validazioni sono avvenute correttamente
-        $updatePswResult = $connessione->updatePassword($email, $new);
-        $connessione->closeDBConnection();
-        if($updatePswResult['success']){
-            if(!$updatePswResult['content']){
-                $_SESSION["update_psw_success_msg"] = 'Password aggiornata con successo!';
-                header("Location: areariservata.php");
-            } else{
-                $_SESSION["update_psw_error_msg"] = 'Non è stato possibile aggiornare la password per problemi tecnici';
-                header("Location: areariservata.php");
-            }
-        } else {
-            header("Location: /500.html");
-            exit();
+else { // il form è gia non valido verificando solo i primi controlli
+    $paginaHTML = str_replace('[email_err]', $emailErr, $paginaHTML);
+    $paginaHTML = str_replace('[old_password_err]', $old_pswErr, $paginaHTML);
+    $paginaHTML = str_replace('[new_password_err]', $new_pswErr, $paginaHTML);
+    echo $paginaHTML;
+}
+
+if($formValido) { //modifica psw solo se tutte le validazioni sono avvenute correttamente
+    $updatePswResult = $connessione->updatePassword($email, $new);
+    $connessione->closeDBConnection();
+    if($updatePswResult['success']){
+        if(!$updatePswResult['content']){
+            $_SESSION["update_psw_success_msg"] = 'Password aggiornata con successo!';
+            header("Location: areariservata.php");
+        } else{
+            $_SESSION["update_psw_error_msg"] = 'Non è stato possibile aggiornare la password per problemi tecnici';
+            header("Location: areariservata.php");
         }
     } else {
-        $paginaHTML = str_replace('[email_err]', $emailErr, $paginaHTML);
-        $paginaHTML = str_replace('[old_password_err]', $old_pswErr, $paginaHTML);
-        $paginaHTML = str_replace('[new_password_err]', $new_pswErr, $paginaHTML);
-        echo $paginaHTML;
+        header("Location: /500.html");
+        exit();
     }
 } else {
-    //utente interagisce con metodo get -> visita la pagina tramite link non tramite form
+    $paginaHTML = str_replace('[email_err]', $emailErr, $paginaHTML);
+    $paginaHTML = str_replace('[old_password_err]', $old_pswErr, $paginaHTML);
+    $paginaHTML = str_replace('[new_password_err]', $new_pswErr, $paginaHTML);
     echo $paginaHTML;
 }
 ?>
