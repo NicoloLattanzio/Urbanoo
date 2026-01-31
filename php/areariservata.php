645f82aa6389f1b4b5a0047edd5c3dab56d00be6
intmainale <alessandro.savio04@gmail.com>
2026-02-01 00:36:49 +0100

    html validated

--- php/areariservata.php
+++ php/areariservata.php
@@ -2,7 +2,7 @@
 session_start();
 
 $paginaHTML = file_get_contents('../html/areariservata.html');
-
+$areariservataMenu = "";
 /*
     User role check:
     admin -> show areariservata.php without wishlist button
@@ -37,14 +37,22 @@ if (!empty($_SESSION['permission']['text'])) {
     $paginaHTML = str_replace(array_keys($placeholders), array_values($placeholders), $paginaHTML);
 }
 $paginaHTML = str_replace("[nome]", $_SESSION['name'], $paginaHTML);
-$paginaHTML = str_replace("[ruolo]", $_SESSION['role'], $paginaHTML);
 
 if ($isAdmin) {
     //admin: remove wishlist button
-    $paginaHTML = str_replace("[wishlist]", "", $paginaHTML);
+    $areariservataMenu = '  <ul id="actionList">
+                                <li><a href="modifica_password.php" id="changepw">Modifica <span lang="en">password</span></a></li>
+                                <li><a href="logout.php" lang="en" id="logout">Logout</a></li>
+                            </ul>';
+    $paginaHTML = str_replace("[areariservata_menu]", $areariservataMenu, $paginaHTML);
 } else {
     //user: add wishlist button
-    $paginaHTML = str_replace("[wishlist]", "<li><a href=\"wishlist.php\" id=\"wishlist\">La mia <span lang=\"en\">wishlist</span></a></li>", $paginaHTML);
+    $areariservataMenu = '  <ul id="actionList">
+                                <li><a href="wishlist.php" id="wishlist">La mia wishlist</a></li>
+                                <li><a href="modifica_password.php" id="changepw">Modifica <span lang="en">password</span></a></li>
+                                <li><a href="logout.php" lang="en" id="logout">Logout</a></li>
+                            </ul>';
+    $paginaHTML = str_replace("[areariservata_menu]", $areariservataMenu, $paginaHTML);
 }
 echo $paginaHTML;
 exit();
